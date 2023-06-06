2dfb25076b6f374b3f26a58b322d175795b5ec82
Guilherme <guilhermebarradasdev@gmail.com>
2023-06-06 16:11:28 -0300

    initial commit

---
+++ greeting.py
@@ -0,0 +1,4 @@
+fnam = input("Posso saber seu primeiro nome, por favor? ")
+lnam = input("Posso saber seu sobrenome, por favor? ")
+print("Obrigado!.")
+print("\nSeu nome é " + fnam + " " + lnam + "\nMuito prazer!")
