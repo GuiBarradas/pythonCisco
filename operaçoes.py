d4dadba5e5bdd187a6e01794b7ee140960ce18cd
Guilherme <guilhermebarradasdev@gmail.com>
2023-06-06 22:37:00 -0300

    exercicios operacoes basicas

---
+++ operaçoes.py
@@ -0,0 +1,9 @@
+a = float(input("Informe o valor de A:"))
+b = float(input("Informe o valor de B:"))
+
+print("O resultado da adição é:" + str(a + b) + "\n")
+print("O resultado da subtração é:" + str(a - b) + "\n")
+print("O resultado da multiplicação é:" + str(a * b) + "\n")
+print("O resultado da divisão é:" + str(a / b))
+print("\nIsso é tudo, pessoal!")
+
